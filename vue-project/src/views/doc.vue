69673e1e470f3e114a65772203b7a7c34caf9876
leigp <907090585@qq.com>
2021-11-10 22:54:11 +0800

    doc边栏样式

--- vue-project/src/views/doc.vue
+++ vue-project/src/views/doc.vue
@@ -6,8 +6,26 @@
      <div class="menu"> </div>
    </div>
    <div class="gulu-content">
-     <aside>边栏</aside>
-     <main>主内容</main>
+     <aside >
+       <h2>组件列表</h2>
+       <ol>
+         <li>
+           <router-link to="/doc/switch">Switch 组件</router-link>
+         </li>
+         <li>
+           <router-link to="/doc/button">Button 组件</router-link>
+         </li>
+         <li>
+           <router-link to="/doc/dialog">Dialog 组件</router-link>
+         </li>
+         <li>
+           <router-link to="/doc/tabs">Tabs 组件</router-link>
+         </li>
+       </ol>
+     </aside>
+     <main>
+       <router-view />
+     </main>
    </div>
 
  </div>
@@ -18,3 +36,53 @@ export default {
   components:{TopNav}
 }
 </script>
+
+<style lang="scss" scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .gulu-content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.gulu-content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
+aside {
+  background: lightblue;
+  width: 150px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 70px 16px 16px;
+  height: 100%;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
+main {
+  overflow: auto;
+}
+</style>
