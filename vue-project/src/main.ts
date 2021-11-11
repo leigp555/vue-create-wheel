c4069dad76c8056a8b73e5b80eebc708b850173c
leigp <907090585@qq.com>
2021-11-12 03:33:31 +0800

    gulu组件自带样式避免用户引用组件样式失效

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,8 +1,8 @@
+import './index.scss'
+import './lib/gulu.scss'
 import {createApp} from 'vue'
 import App from './App.vue'
-import './index.scss'
 import {router} from "./route";
-
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
