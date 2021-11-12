88f3f7e19c8ee24905524ba675e1b8684c7401d1
leigp <907090585@qq.com>
2021-11-13 02:32:15 +0800

    tab组件初始化

--- vue-project/src/components/tabDemo.vue
+++ vue-project/src/components/tabDemo.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    我是tab组件
+    <Tabs>
+      <Tab >页面一</Tab>
+      <Tab>页面二</Tab>
+    </Tabs>
   </div>
 </template>
+<script lang="ts">
+import Tab from '../lib/tab.vue'
+import Tabs from '../lib/tabs.vue'
+export default {
+  components:{Tab,Tabs}
+}
+</script>
---
+++ vue-project/src/lib/tabs.vue
@@ -0,0 +1,23 @@
+<template>
+  tabs
+  <div>
+    <component v-for="(item,index) in defaults" :is="item" :key="index"></component>
+  </div>
+</template>
+<script lang="ts">
+import Tab from "./tab.vue"
+
+export default {
+  components: {Tab},
+  setup(props, context) {
+    const {slots}=context
+    const defaults = slots.default()
+    defaults.forEach((item)=>{
+      if(item.type!==Tab){
+        throw new Error("子组件类型必须是Tab")
+      }
+    })
+    return {defaults}
+  }
+}
+</script>
