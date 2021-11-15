442c17d60b963e8e850d777b81b26517a65aab8c
leigp <907090585@qq.com>
2021-11-16 04:03:32 +0800

    完善主题

--- vue-project/src/lib/tab.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <slot />
-</template>
-<script lang="ts">
-export default {
-  inheritAttrs: false     //消除title没有被解析的警告
-}
-</script>
--- vue-project/src/main.ts
+++
@@ -1,8 +0,0 @@
-import './index.scss'
-import './lib/gulu.scss'
-import {createApp} from 'vue'
-import App from './App.vue'
-import {router} from "./route";
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
