7e0c69e4bc1cb5e0eed6424a7b1f5b7c57accba0
leigp <907090585@qq.com>
2021-11-17 18:23:29 +0800

    引入markdown插件并完成文档部分内容

--- src/view/install.vue
+++ src/view/install.vue
@@ -1,26 +1,22 @@
 <template>
- <article class="markdown-body">
-   <h2>安装</h2>
-   <p>打开终端运行下列命令：</p>
-
-   <code>npm install king-ui</code>
-
-   <p>或</p>
-
-   <code>yarn add king-ui</code>
-   <br>
-   <p>下一节：
-     <router-link to="/doc/get-started">开始使用</router-link>
-   </p>
- </article>
+  <article class="markdown-body" v-html="Install">
+  </article>
 </template>
 <script lang="js">
+import Install from "../markdown/install.md"
+export default {
+  setup(){
+    return {Install}
+  }
+}
 </script>
+
 <style lang="scss" scoped>
 .markdown-body {
   background-color: #fffdf1;
   padding: 20px;
-  >code{
+
+  > code {
   }
 }
 </style>
--- src/view/start.vue
+++ src/view/start.vue
@@ -1,3 +1,20 @@
 <template>
-  <div> start</div>
+  <article class="markdown-body" v-html="Start">
+  </article>
 </template>
+<script>
+import Start from "../markdown/get-started.md"
+export default {
+  setup(){
+    return {Start}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.markdown-body {
+  background-color: #fffdf1;
+  padding: 20px;
+}
+</style>
