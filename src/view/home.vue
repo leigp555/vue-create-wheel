19a5ceb0c5e47b4b8bc1c66fa5cdd0618b026613
leigp <907090585@qq.com>
2021-11-20 02:10:09 +0800

    首页优化

--- src/view/home.vue
+++ src/view/home.vue
@@ -7,37 +7,56 @@
         </svg>
       </div>
       <ol class="doc">
-        <li><a href="mailto:907090585@qq.com">联系我</a> </li>
-        <li><router-link to="/doc">文档</router-link></li>
+        <li><a href="mailto:907090585@qq.com">联系我</a></li>
+        <li>
+          <router-link to="/doc">文档</router-link>
+        </li>
       </ol>
     </nav>
     <main class="gulu-homeContent">
       <h1>Useful Component</h1>
       <h4>基于vue和TS开发的一套组件</h4>
       <div class="link">
-        <a href="https://github.com"><button>gitHub</button> </a>
-        <router-link to="/doc"><button>开始使用</button></router-link>
+        <a href="https://github.com">
+          <button>gitHub</button>
+        </a>
+        <router-link to="/doc">
+          <button>开始使用</button>
+        </router-link>
       </div>
     </main>
     <footer class="gulu-homeFeature">
       <div class="content">
-        <div class="vue">vue</div>
-        <div class="ts">ts</div>
-        <div class="into">简介</div>
+        <div class="vue">
+          <svg class="icon-feature" aria-hidden="true">
+            <use xlink:href="#icon-Vue"></use>
+          </svg>
+          <p>基于Vue3开发</p>
+        </div>
+        <div class="ts">
+          <svg class="icon-feature" aria-hidden="true">
+            <use xlink:href="#icon-typescript"></use>
+          </svg>
+          <p>采用Typescript书写</p>
+        </div>
+        <div class="into">
+          <svg class="icon-feature" aria-hidden="true">
+            <use xlink:href="#icon-easy"></use>
+          </svg>
+          <p>代码简单易懂</p>
+        </div>
       </div>
     </footer>
   </div>
 </template>
 <script lang="ts">
-export default {
-
-}
+export default {}
 </script>
 <style lang="scss" scoped>
 $color: #ffe3b7;
 .gulu-homePage {
   > nav {
-    background-color:$color;
+    background-color: $color;
     padding: 30px 10vw 0;
     height: 50px;
     display: flex;
@@ -46,21 +65,25 @@ $color: #ffe3b7;
     @media(max-width: 400px) {
       justify-content: center;
     }
+
     > ol {
       display: flex;
       white-space: nowrap;
       @media(max-width: 400px) {
-       display: none;
+        display: none;
       }
-      >li{
-        >a{
+
+      > li {
+        > a {
           text-decoration: none;
-          &:hover{
+
+          &:hover {
             padding-top: 10px;
-           border-top: 2px solid #fffdf1;
+            border-top: 2px solid #fffdf1;
           }
         }
       }
+
       & :first-child {
         margin-right: 15px;
       }
@@ -79,6 +102,7 @@ $color: #ffe3b7;
     @media (max-width: 400px) {
       padding-top: 40px;
     }
+
     > h1 {
       font-size: 48px;
       @media (max-width: 400px) {
@@ -94,8 +118,10 @@ $color: #ffe3b7;
       @media (max-width: 400px) {
         margin-bottom: 80px;
       }
-      >a{
+
+      > a {
         text-decoration: none;
+
         > button {
           color: #fafafa;
           background-color: #111111;
@@ -104,7 +130,7 @@ $color: #ffe3b7;
           width: 7em;
           border-radius: 1em;
           border: none;
-      }
+        }
       }
     }
   }
@@ -112,30 +138,44 @@ $color: #ffe3b7;
   > footer {
     > .content {
       margin-top: 80px;
-      display:flex;
+      display: flex;
       gap: 5vw;
       justify-content: center;
       flex-wrap: wrap;
       @media (max-width: 400px) {
         margin-top: 60px;
       }
+
       > div {
         background-color: $color;
         border-radius: 20px;
         padding: 10px;
-        width:200px;
-        height:200px;
+        width: 200px;
+        height: 200px;
+        display: flex;
+        gap: 10px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
         @media (max-width: 400px) {
           height: 110px;
+        }
+        >p{
+          font-size: 20px;
         }
       }
     }
   }
   .icon {
-    width: 40px; height: 34px;
+    width: 40px;
+    height: 34px;
     vertical-align: -0.15em;
     fill: currentColor;
     overflow: hidden;
+  }
+  .icon-feature{
+    width: 80px;
+    height: 80px;
   }
 }
 </style>
