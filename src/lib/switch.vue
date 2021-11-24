b2fa0ebee60d85b98f53bfca5a3f95bd5b8d4ecf
leigp <907090585@qq.com>
2021-11-24 10:46:19 +0800

    总体优化，以及创建index.ts

--- src/lib/switch.vue
+++ src/lib/switch.vue
@@ -7,6 +7,7 @@
 </template>
 <script lang="ts">
 export default {
+  name: "Switch",
   props: {
     value: Boolean,
     disabled: {
@@ -52,7 +53,7 @@ $h2: $h - 4px;
     background: #1890ff;
 
     > span {
-      left: calc(100% - #{$h2} - 2px);
+      left: 24px;
     }
   }
 
